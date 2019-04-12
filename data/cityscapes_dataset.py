479d41c7f4db3996dbab5dccba573d6c8646024e
Taesung Park <taesung89@gmail.com>
2019-04-12 13:39:45 -0700

    fixed a bug in cityscapes_dataset.py as suggested by @wasd96040501

--- data/cityscapes_dataset.py
+++ data/cityscapes_dataset.py
@@ -11,6 +11,7 @@ class CityscapesDataset(Pix2pixDataset):
 
     @staticmethod
     def modify_commandline_options(parser, is_train):
+        parser = Pix2pixDataset.modify_commandline_options(parser, is_train)
         parser.set_defaults(preprocess_mode='fixed')
         parser.set_defaults(load_size=512)
         parser.set_defaults(crop_size=512)
