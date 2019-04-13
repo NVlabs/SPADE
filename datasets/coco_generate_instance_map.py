c3e6dade46452d97a21da9ecf9bc6d656a50ad18
Taesung Park <taesung89@gmail.com>
2019-04-12 17:49:03 -0700

    added copyright

--- datasets/coco_generate_instance_map.py
+++ datasets/coco_generate_instance_map.py
@@ -1,3 +1,8 @@
+"""
+Copyright (C) 2019 NVIDIA Corporation.  All rights reserved.
+Licensed under the CC BY-NC-SA 4.0 license (https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode).
+"""
+
 import os
 import argparse
 from pycocotools.coco import COCO
